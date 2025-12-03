dd76cb91112d8387e7ef2f02882f0c54e448d031
MorkZar <gmorquecho12579@gmail.com>
2025-12-03 15:52:27 -0600

    fix peticiones

--- app/peticiones.php
+++ app/peticiones.php
@@ -13,7 +13,7 @@ if(!isset($_SESSION['usuario'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/peticiones.css">
-    <title>Peticiones</title>
+    <title>Peticiones Pendientes</title>
 </head>
 
 <!-- Alerta de éxito -->
